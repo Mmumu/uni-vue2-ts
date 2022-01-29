d3625e4385d0e465d0fc0562462e5677d54e18ba
xiebin <bin.xie@betterwood.com>
2022-01-29 15:50:54 +0800

    feat: uniapp插件配置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Avatar from './components/avatar/avatar.vue'
+Vue.component('avatar', Avatar)
+
 Vue.config.productionTip = false
 
 new App().$mount()
---
+++ src/pages/word.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>Hello plugin
+  </view>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      test: false
+    }
+  },
+  methods: {
+    aaa() {
+      uni.showToast({
+        title: '收到'
+      })
+    }
+  }
+}
+</script>
